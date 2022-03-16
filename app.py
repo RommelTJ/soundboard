acefd297a0a6d0e3ce0913a82f75c59330406013
Rommel Rico <rommel@chatmeter.com>
2022-03-16 16:54:21 -0700

    :sparkles: Soundboard: Spaghet

--- app.py
+++ app.py
@@ -15,5 +15,11 @@ def roasted():
     return ''
 
 
+@app.route('/spaghet', methods=['POST'])
+def roasted():
+    play("./sounds/somebody-toucha-my-spaghet.mp3")
+    return ''
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5001')
