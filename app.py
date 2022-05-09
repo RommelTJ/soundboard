33921690646d30dcda9e32f85a30c5b436b020d5
Rommel Rico <rommel@chatmeter.com>
2022-05-08 20:32:54 -0700

    :sparkles: Sounds: New endpoints

--- app.py
+++ app.py
@@ -26,6 +26,36 @@ def forest_fires():
     play("./sounds/forest-fires.mp3")
     return ''
 
+@app.route('/mr-worldwide', methods=['POST'])
+def forest_fires():
+    play("./sounds/mr-worldwide.mp3")
+    return ''
+
+@app.route('/boots-of-escaping', methods=['POST'])
+def forest_fires():
+    play("./sounds/boots-of-escaping.mp3")
+    return ''
+
+@app.route('/friday', methods=['POST'])
+def forest_fires():
+    play("./sounds/friday.mp3")
+    return ''
+
+@app.route('/happy-friday-jr', methods=['POST'])
+def forest_fires():
+    play("./sounds/happy-friday-jr.mp3")
+    return ''
+
+@app.route('/lightning-bolt', methods=['POST'])
+def forest_fires():
+    play("./sounds/lightning-bolt.mp3")
+    return ''
+
+@app.route('/magic-missile', methods=['POST'])
+def forest_fires():
+    play("./sounds/magic-missile.mp3")
+    return ''
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5001')
